6033c9f329ad082d2f6593602089b23266662c91
TheEntropyShard <theentropyshard@gmail.com>
2024-08-24 00:10:04 +0300

    Refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+# DynamicTitle
+> Makes your Cosmic Reach window title change when you switch worlds!
+
+## Quick Start
+For building, the usual `gradle build` task can be used. The output will be in the `build/libs/` folder
--- src/main/java/me/theentropyshard/dynamictitle/DynamicTitleMod.java
+++ src/main/java/me/theentropyshard/dynamictitle/DynamicTitleMod.java
@@ -1,16 +1,16 @@
-package com.example.example_mod;
+package me.theentropyshard.dynamictitle;
 
 import dev.crmodders.cosmicquilt.api.entrypoint.ModInitializer;
 import org.quiltmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+public class DynamicTitleMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("DynamicTitle");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Example Mod Initialized!");
+		LOGGER.info("DynamicTitle initialized!");
 	}
 }
 
